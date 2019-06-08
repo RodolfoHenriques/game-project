c6ad81f86fb69915a3e020519b006681d303ddab
rodolfohenriques <rodolfo.henriques.uc@gmail.com>
2019-06-08 22:40:24 +0100

    moveTank() Added

---
+++ game-project/src/org/academiadecodigo/allpainnogain/gameproject/Direction.java
@@ -0,0 +1,13 @@
+package org.academiadecodigo.allpainnogain.gameproject;
+
+public enum Direction {
+    NORTH,
+    SOUTH,
+    EAST,
+    WEST,
+    NORTHEAST,
+    NORTHWEST,
+    SOUTHEAST,
+    SOUTHWEST
+
+}
--- game-project/src/org/academiadecodigo/allpainnogain/gameproject/Tank.java
+++ game-project/src/org/academiadecodigo/allpainnogain/gameproject/Tank.java
@@ -1,17 +1,108 @@
 package org.academiadecodigo.allpainnogain.gameproject;
 
+
 public class Tank {
 
     private int health;
     private int speed;
-    private Position tankPosition;
     private boolean destroyed;
+    private Position tankPos;
+    private int direction;
 
-    public Tank(Position tankPosition) {
-        this.tankPosition = tankPosition;
+    public Tank(Position tankPos) {
+        this.tankPos = tankPos;
     }
 
     public boolean isDestroyed() {
         return destroyed;
     }
+
+    public void moveTank() {
+        int currentTankPosX = tankPos.getX();
+        int currentTankPosY = tankPos.getY();
+        int step = 1;
+        Direction dir = Direction.values()[direction];
+
+        switch (dir) {
+            case NORTH:
+                if (currentTankPosY - 1 < BattleField.MARGIN) {
+                    break;
+                }
+                currentTankPosY -= step;
+                break;
+            case SOUTH:
+                if (currentTankPosY + 1 > (BattleField.HEIGHT + BattleField.MARGIN - tankPos.getTankRectangle().getWidth())) {
+                    break;
+                }
+                currentTankPosY += step;
+                break;
+
+            case EAST:
+                if (currentTankPosX + 1 > (BattleField.WIDTH + BattleField.MARGIN - tankPos.getTankRectangle().getWidth())) {
+                    break;
+                }
+                currentTankPosX += step;
+                break;
+
+            case WEST:
+                if (currentTankPosX - 1 < BattleField.MARGIN) {
+                    break;
+                }
+                currentTankPosX -= step;
+                break;
+
+            case SOUTHWEST:
+                if (currentTankPosX - 1 < BattleField.MARGIN) {
+                    break;
+                }
+                currentTankPosX -= step;
+                currentTankPosY += step;
+                break;
+
+            case NORTHEAST:
+                if (currentTankPosX + 1 > (BattleField.WIDTH + BattleField.MARGIN - tankPos.getTankRectangle().getWidth())) {
+                    break;
+                }
+                currentTankPosX += step;
+                currentTankPosY -= step;
+                break;
+
+            case SOUTHEAST:
+                if (currentTankPosX + 1 > (BattleField.WIDTH + BattleField.MARGIN - tankPos.getTankRectangle().getWidth())) {
+                    break;
+                }
+                currentTankPosX += step;
+                currentTankPosY += step;
+                break;
+
+            case NORTHWEST:
+                if (currentTankPosX - 1 < BattleField.MARGIN) {
+                    break;
+                }
+                currentTankPosX -= step;
+                currentTankPosY -= step;
+                break;
+        }
+
+
+        tankPos.getTankRectangle().translate(currentTankPosX - tankPos.getX(), currentTankPosY - tankPos.getY());
+
+        tankPos.setX(currentTankPosX);
+        tankPos.setY(currentTankPosY);
+
+    }
+
+    public int getDirection() {
+        return direction;
+    }
+
+    public Position getTankPos() {
+        return tankPos;
+    }
+
+    public void setDirection(int direction) {
+        this.direction = direction;
+    }
+
+
 }
