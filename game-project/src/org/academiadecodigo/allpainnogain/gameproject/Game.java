e7071da4373acce63741104b1e5b9b3f4283ef39
pedroferraz <ferraz.pedro.pf@gmail.com>
2019-06-08 23:32:32 +0100

    added Game constructor and start() in Game

--- game-project/src/org/academiadecodigo/allpainnogain/gameproject/Game.java
+++ game-project/src/org/academiadecodigo/allpainnogain/gameproject/Game.java
@@ -6,19 +6,35 @@ import org.academiadecodigo.allpainnogain.gameproject.Players.Player2;
 
 public class Game {
 
+    private int delay;
+
     private BattleField battleField;
     private Player player1;
     private Player player2;
 
     // default constructor
+    public Game(int delay){
+        this.delay = delay;
+    }
 
 
     public void init() {
         battleField = new BattleField();
         battleField.init();
-        player1 = new Player1("P1", new Tank(new Position(123,123)));
-        player2 = new Player2("P2", new Tank(new Position(300,300)));
+        player1 = new Player1("P1", new Tank(new Position(1700, 900)));
+        //player2 = new Player2("P2", new Tank(new Position(300, 300)));
 
     }
 
+
+    public void start()throws InterruptedException{
+
+        while (true) {
+            Thread.sleep(delay);
+            if(player1.isKeyPressed()){
+                ((Player1) player1).moveTank();
+            }
+
+        }
+    }
 }
--- game-project/src/org/academiadecodigo/allpainnogain/gameproject/Main.java
+++ game-project/src/org/academiadecodigo/allpainnogain/gameproject/Main.java
@@ -1,10 +1,11 @@
 package org.academiadecodigo.allpainnogain.gameproject;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException{
 
-        Game game = new Game();
+        Game game = new Game(50);
         game.init();
+        game.start();
 
     }
 }
