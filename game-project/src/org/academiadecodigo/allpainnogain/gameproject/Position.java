61e403882c97f5b14ea47681f864dd5e623943fb
José Pedro Dias Cristóvão <jp.cristovao1@hotmail.com>
2019-06-08 20:19:12 +0100

    Position added getx, gety,setx, sety, getTankRectangle

--- game-project/src/org/academiadecodigo/allpainnogain/gameproject/Position.java
+++ game-project/src/org/academiadecodigo/allpainnogain/gameproject/Position.java
@@ -14,4 +14,25 @@ public class Position {
         tankRectangle = new Rectangle(x,y,20,20); //Test
         tankRectangle.fill();                              //Test
     }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public Rectangle getTankRectangle() {
+        return tankRectangle;
+    }
+}
 }
