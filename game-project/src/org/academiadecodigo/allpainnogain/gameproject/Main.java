493515b1761f153d2c11e8c1afceb7ec2e503db1
claudia <anaclaudiafaria@live.com.pt>
2019-06-10 03:39:53 +0100

    added Menu options

--- game-project/src/org/academiadecodigo/allpainnogain/gameproject/Main.java
+++ game-project/src/org/academiadecodigo/allpainnogain/gameproject/Main.java
@@ -1,12 +1,25 @@
 package org.academiadecodigo.allpainnogain.gameproject;
 
+import org.academiadecodigo.allpainnogain.gameproject.Screens.Menu;
+import org.academiadecodigo.simplegraphics.keyboard.KeyboardEvent;
+import org.academiadecodigo.simplegraphics.keyboard.KeyboardHandler;
+
 public class Main {
 
     public static void main(String[] args) throws InterruptedException{
 
+       // KeyboardEvent keyboardEvent = null;
+
         Game game = new Game(15);
 
-        game.init();
-        game.start();
+        Menu menu = new Menu();
+
+
+        while(true){
+
+        }
+
+       // game.init();
+        //game.start();
     }
 }
--- game-project/src/org/academiadecodigo/allpainnogain/gameproject/Screens/Menu.java
+++ game-project/src/org/academiadecodigo/allpainnogain/gameproject/Screens/Menu.java
@@ -1,19 +1,30 @@
 package org.academiadecodigo.allpainnogain.gameproject.Screens;
 
 import org.academiadecodigo.simplegraphics.graphics.Rectangle;
+import org.academiadecodigo.simplegraphics.keyboard.Keyboard;
 import org.academiadecodigo.simplegraphics.keyboard.KeyboardEvent;
 import org.academiadecodigo.simplegraphics.keyboard.KeyboardEventType;
 import org.academiadecodigo.simplegraphics.keyboard.KeyboardHandler;
 import org.academiadecodigo.simplegraphics.pictures.Picture;
 
-public class Menu extends Picture implements KeyboardHandler {
 
+public class Menu implements KeyboardHandler {
 
     private Picture picture = new Picture(0, 0, "menuImage.png");
+    private Keyboard keyboard;
+
+    private Rectangle selectMenuStart = new Rectangle(80, 50, 305, 120);
+    private Rectangle selectMenuGuide = new Rectangle(80, 185, 305, 120);
 
     public Menu() {
+
+        keyboard = new Keyboard(this);
         initKeyboard();
+
+        picture.draw();
+        selectMenuStart.draw();
     }
+
 
     public void initKeyboard() {
 
@@ -24,16 +35,19 @@ public class Menu extends Picture implements KeyboardHandler {
         KeyboardEvent down = new KeyboardEvent();
         down.setKey(KeyboardEvent.KEY_DOWN);
         down.setKeyboardEventType(KeyboardEventType.KEY_PRESSED);
+
+        KeyboardEvent enter = new KeyboardEvent();
+        enter.setKey(KeyboardEvent.KEY_ENTER);
+        enter.setKeyboardEventType(KeyboardEventType.KEY_PRESSED);
+
+        keyboard.addEventListener(up);
+        keyboard.addEventListener(down);
+        keyboard.addEventListener(enter);
     }
 
 
     @Override
     public void keyPressed(KeyboardEvent keyboardEvent) {
-
-        Rectangle selectMenuStart = new Rectangle(80, 50, 305, 120);
-        Rectangle selectMenuGuide = new Rectangle(80, 185, 305, 120);
-
-        selectMenuStart.draw();
 
         switch (keyboardEvent.getKey()) {
 
@@ -46,11 +60,16 @@ public class Menu extends Picture implements KeyboardHandler {
                 selectMenuStart.delete();
                 selectMenuGuide.draw();
                 break;
+
+            case KeyboardEvent.KEY_ENTER:
+
+                break;
         }
     }
 
+
     @Override
-    public void keyReleased (KeyboardEvent keyboardEvent){
+    public void keyReleased(KeyboardEvent keyboardEvent) {
 
     }
 }
