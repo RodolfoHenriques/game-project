c8748a972f4730016ac1491691967fe001d912ee
claudia <anaclaudiafaria@live.com.pt>
2019-06-14 00:54:18 +0100

    changed goat's position

--- game-project/src/org/academiadecodigo/allpainnogain/gameproject/BattleField.java
+++ game-project/src/org/academiadecodigo/allpainnogain/gameproject/BattleField.java
@@ -19,29 +19,29 @@ public class BattleField {
         battleFieldRectangle.draw();
 
 
-        Obstacle obstacle1 = new Obstacle(WIDTH/8, HEIGHT/4, "fiveTrees.png");
+        Obstacle obstacle1 = new Obstacle(WIDTH / 8, HEIGHT / 4, "fiveTrees.png");
         obstacle1.drawObstacle();
 
-        Obstacle obstacle2 = new Obstacle(WIDTH/4, HEIGHT/2, "nineTrees.png");
+        Obstacle obstacle2 = new Obstacle(WIDTH / 4, HEIGHT / 2, "nineTrees.png");
         obstacle2.drawObstacle();
 
-        Obstacle obstacle3 = new Obstacle(WIDTH - WIDTH/5, HEIGHT - HEIGHT/5+20, "fiveTrees.png");
+        Obstacle obstacle3 = new Obstacle(WIDTH - WIDTH / 5, HEIGHT - HEIGHT / 5 + 20, "fiveTrees.png");
         obstacle3.drawObstacle();
 
-        Obstacle obstacle4 = new Obstacle(WIDTH-WIDTH/6-20, HEIGHT/5, "nineTrees.png");
+        Obstacle obstacle4 = new Obstacle(WIDTH - WIDTH / 6 - 20, HEIGHT / 5, "nineTrees.png");
         obstacle4.drawObstacle();
 
 
-        Goat goat1 = new Goat(WIDTH/2+25, HEIGHT/4, "goat.png");
+        Goat goat1 = new Goat(WIDTH / 2 + 25, HEIGHT / 4, "goat.png");
         goat1.drawObstacle();
 
-        Goat goat2 = new Goat(WIDTH/2 +25, HEIGHT-HEIGHT/6, "goat.png");
+        Goat goat2 = new Goat(WIDTH / 2 + 25, HEIGHT + 30 - HEIGHT / 6, "goat.png");
         goat2.drawObstacle();
 
-        Obstacle obstacle8 = new Obstacle(WIDTH/7 - 25, HEIGHT-HEIGHT/7-20, "rock.png");
+        Obstacle obstacle8 = new Obstacle(WIDTH / 7 - 25, HEIGHT - HEIGHT / 7 - 20, "rock.png");
         obstacle8.drawObstacle();
 
-        Obstacle obstacle9 = new Obstacle(WIDTH - WIDTH/12, HEIGHT /4, "rock.png");
+        Obstacle obstacle9 = new Obstacle(WIDTH - WIDTH / 12, HEIGHT / 4, "rock.png");
         obstacle9.drawObstacle();
 
 
